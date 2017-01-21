0388811e72a8db8502631402ee05eded740ba2e0
Liviu Rau <liviu_aurelian@yahoo.com>
2017-01-21 21:31:29 +0200

    buba

--- PI/compile.sh
+++ PI/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 docker rm acompiler
 docker rmi arduino-compiler
-docker build -t arduino-compiler
-docker run 
-	--name acompiler /
-	-h acompiler /
-	-it /
-	-v /home/pi/rpd:/home/pi/rpd /
-	--group-add dialout /
-	--device=/dev/ttyUSB0:/dev/ttyUSB0:rw / 
+docker build -t arduino-compiler .
+docker run \
+	--name acompiler \
+	-h acompiler \
+	-it \
+	-v /home/pi/rpd:/home/pi/rpd \
+	--group-add dialout \
+	--device=/dev/ttyUSB0:/dev/ttyUSB0:rw \
 	arduino-compiler
